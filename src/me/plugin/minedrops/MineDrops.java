d25d711fe0846a82d76104966357dda89245870c
BuildTools <unconfigured@null.spigotmc.org>
2017-01-13 06:20:49 -0500

    updating more code

--- src/me/plugin/minedrops/MineDrops.java
+++ src/me/plugin/minedrops/MineDrops.java
@@ -11,8 +11,6 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Random;
-
 public class MineDrops extends JavaPlugin implements Listener{
 
     @Override
@@ -27,21 +25,24 @@ public class MineDrops extends JavaPlugin implements Listener{
         Block b = e.getBlock();
         Player p = e.getPlayer();
 
-        Random rand = new Random();
-        int num = rand.nextInt(101);
-        int pct;
-        int pctTotal = 0;
+        int getIron, getGold, getDiamond;
+        int pctIron, pctGold, pctDiamond;
 
-        if (num == 101) {
-            num = rand.nextInt(101);
-        } else {
-            pctTotal = (num * 100 / 100);
-        }
-        pct = getConfig().getInt("ironpct");
+        getIron = getConfig().getInt("ironpct");
+        getGold = getConfig().getInt("goldpct");
+        getDiamond = getConfig().getInt("diamondpct");
+
+        pctIron = getRandom(1, getIron + 20);
+        pctGold = getRandom(1, getGold + 20);
+        pctDiamond = getRandom(1, getDiamond + 20);
 
         if (b.getType() == Material.IRON_ORE) {
-            p.sendMessage(ChatColor.DARK_AQUA + "You have mined an Iron Ore! and the perecntage was " + pctTotal + " and the base perectnage is " + pct);
-            if (pctTotal == pct) { b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.GLOWSTONE_DUST)); }
+            p.sendMessage(ChatColor.DARK_AQUA + "You have mined an Iron Ore! and the perecntage was " + pctIron + " and the base perectnage is " + getIron);
+            if (pctIron == getIron) { b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.GLOWSTONE_DUST)); }
         }
+    }
+
+    public int getRandom(int min, int max) {
+        return (int)(Math.random() * (max - min)) + min;
     }
 }
