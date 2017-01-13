6e1dff9176b1e620ef15ae06dffc7db321880e07
vertex101 <vertex101@gmx.com>
2017-01-13 02:51:18 -0500

    working on adding in commands for adding and removing items to the list within the config for each ore

--- src/me/plugin/minedrops/MineDrops.java
+++ src/me/plugin/minedrops/MineDrops.java
@@ -5,14 +5,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Random;
 
 public class MineDrops extends JavaPlugin implements Listener{
 
@@ -23,26 +23,60 @@ public class MineDrops extends JavaPlugin implements Listener{
         saveConfig();
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+        if (cmd.getName().equalsIgnoreCase("minedrop")) {
+            if (args[0].equalsIgnoreCase("add")){
+                if (args[1].equalsIgnoreCase("iron")) {
+                    sender.sendMessage("This is a Test Command of iron!!!!");
+                } else if (args[1].equalsIgnoreCase("gold")) {
+
+                } else if (args[1].equalsIgnoreCase("diamond")) {
+
+                } else {
+                    sender.sendMessage("Please use /md add then either select iron, gold, or diamond");
+                    return true;
+                }
+            } else {
+                sender.sendMessage("Please use the following /minedrop or /md then add");
+                return true;
+            }
+
+        } else {
+            sender.sendMessage("Please use the following /minedrop or /md then add");
+            return true;
+        }
+        return true;
+    }
+
     @EventHandler
     public void onBreak(BlockBreakEvent e) {
         Block b = e.getBlock();
         Player p = e.getPlayer();
 
-        Random rand = new Random();
-        int num = rand.nextInt(101);
-        int pct;
-        int pctTotal = 0;
+        int getIron, getGold, getDiamond;
+        int pctIron, pctGold, pctDiamond;
 
-        if (num == 101) {
-            num = rand.nextInt(101);
-        } else {
-            pctTotal = (num * 100 / 100);
-        }
-        pct = getConfig().getInt("ironpct");
+        getIron = getConfig().getInt("ironpct");
+        getGold = getConfig().getInt("goldpct");
+        getDiamond = getConfig().getInt("diamondpct");
+
+        pctIron = getRandom(1, getIron + 20);
+        pctGold = getRandom(1, getGold + 20);
+        pctDiamond = getRandom(1, getDiamond + 20);
 
         if (b.getType() == Material.IRON_ORE) {
-            p.sendMessage(ChatColor.DARK_AQUA + "You have mined an Iron Ore! and the perecntage was " + pctTotal + " and the base perectnage is " + pct);
-            if (pctTotal == pct) { b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.GLOWSTONE_DUST)); }
+            p.sendMessage(ChatColor.DARK_AQUA + "You have mined an Iron Ore! and the perecentage was " + pctIron + " and the base perectnage is " + getIron);
+            if (pctIron == getIron) { b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.GLOWSTONE_DUST)); }
+        } else if (b.getType() == Material.GOLD_ORE) {
+            p.sendMessage(ChatColor.DARK_AQUA + "You have mined an Iron Ore! and the perecentage was " + pctGold + " and the base perectnage is " + getGold);
+        } else if (b.getType() == Material.DIAMOND_ORE) {
+            p.sendMessage(ChatColor.DARK_AQUA + "You have mined an Iron Ore! and the perecentage was " + pctDiamond + " and the base perectnage is " + getDiamond);
         }
+    }
+
+    public int getRandom(int min, int max) {
+        return (int)(Math.random() * (max - min)) + min;
     }
 }
