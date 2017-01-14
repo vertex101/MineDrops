7355f18f89f2fb81e4643e8884589771ce52e727
BuildTools <unconfigured@null.spigotmc.org>
2017-01-13 19:52:48 -0500

    ajusted more code and added in more ore

--- src/me/plugin/minedrops/MineDrops.java
+++ src/me/plugin/minedrops/MineDrops.java
@@ -26,29 +26,50 @@ public class MineDrops extends JavaPlugin implements Listener{
         Block b = e.getBlock();
         Player p = e.getPlayer();
 
-        int getIron, getGold, getDiamond, pctIron, pctGold, pctDiamond, pctCoal, getMaterial;
+        int getIron, getGold, getDiamond, getLapis, getEmerald, pctIron, pctGold, pctDiamond, pctCoal, pctLapis, pctEmerald;
 
         getIron = getConfig().getInt("ironpct");
         getGold = getConfig().getInt("goldpct");
         getDiamond = getConfig().getInt("diamondpct");
+        getLapis = getConfig().getInt("lapispct");
+        getEmerald = getConfig().getInt("emeraldpct");
 
-        pctIron = getRandom(1, getIron + 20);
-        pctGold = getRandom(1, getGold + 20);
-        pctDiamond = getRandom(1, getDiamond + 20);
+        pctIron = getRandom(1, getIron + 10);
+        pctGold = getRandom(1, getGold + 10);
+        pctDiamond = getRandom(1, getDiamond + 10);
+        pctLapis = getRandom(1, getLapis + 10);
+        pctEmerald = getRandom(1, getEmerald + 10);
         pctCoal = getRandom(1, 15);
         Material[] grabMats = Material.values();
         ItemStack item = new ItemStack(grabMats[getRandom(0, grabMats.length)]);
 
-        if (b.getType() == Material.IRON_ORE) {
-            p.sendMessage(ChatColor.DARK_AQUA + "You have mined an Iron Ore! and the perecntage was " + pctIron + " and the base perectnage is " + getIron);
-            if (pctIron == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
-            if (pctIron <= getIron) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
-        } else if (b.getType() == Material.GOLD_ORE) {
-            if (pctGold == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+        if (b.getType() == Material.COAL_ORE) {
+            if (pctCoal == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctCoal == 4) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
         } else if (b.getType() == Material.DIAMOND_ORE) {
             if (pctDiamond == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
-        } else if (b.getType() == Material.COAL_ORE) {
-            if (pctCoal == 4) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctDiamond <= getDiamond) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
+        } else if (b.getType() == Material.EMERALD_ORE) {
+            if (pctEmerald == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctEmerald <= getEmerald) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
+        } else if (b.getType() == Material.GOLD_ORE) {
+            if (pctGold == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctGold <= getGold) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
+        } else if (b.getType() == Material.GLOWING_REDSTONE_ORE) {
+            if (pctDiamond == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctDiamond <= getDiamond) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
+        } else if (b.getType() == Material.IRON_ORE) {
+            if (pctIron == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctIron <= getIron) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
+        } else if (b.getType() == Material.LAPIS_ORE) {
+            if (pctLapis == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctLapis <= getLapis) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
+        } else if (b.getType() == Material.REDSTONE_ORE) {
+            if (pctDiamond == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctDiamond <= getDiamond) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
+        } else if (b.getType() == Material.QUARTZ_ORE) {
+            if (pctIron == 1) { b.getWorld().createExplosion(b.getLocation(), 4.0F); }
+            if (pctIron <= getIron) { b.getWorld().dropItemNaturally(b.getLocation(), item); }
         }
     }
     public int getRandom(int min, int max) {
